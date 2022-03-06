7b2293624f084f4be11089974f91ac7d2fe939d5
sadra <sadrataghavi1383@gmail.com>
2022-03-06 22:04:15 +0330

    a test axios post

--- React/src/App.tsx
+++ React/src/App.tsx
@@ -1,6 +1,20 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+
+// cookies
+// import { get as GetCookies } from 'js-cookie'
+
+// axios
+import axios from 'axios'
 
 const App = () => {
+    useEffect(() => {
+        axios
+            .post('/api/test/', {
+                name: 'test',
+            })
+            .then(res => console.log(res))
+    }, [])
+
     return <div>App</div>
 }
 
