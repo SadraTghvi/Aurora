f3a5f441f2f148b5121e49e5c7d0758cd2206d34
sadra <sadrataghavi1383@gmail.com>
2022-03-07 16:25:16 +0330

    initiated home page/hero section

---
+++ React/src/pages/Home/components/HeroSection.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+// style
+import './style/herosection.scss'
+
+const HeroSection = () => {
+    return <div>HeroSection</div>
+}
+
+export default HeroSection
---
+++ React/src/pages/Home/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+// sections
+import HeroSection from './components/HeroSection'
+
+// style
+import './style/home.scss'
+
+const Home = () => {
+    return (
+        <main className='home'>
+            <HeroSection />
+        </main>
+    )
+}
+
+export default Home
