d0500e744d69542a91df3f488521aa46f636b413
sadra <sadrataghavi1383@gmail.com>
2022-03-07 17:15:47 +0330

    iam A FKING GENUIS

--- React/src/pages/Home/components/HeroSection.tsx
+++ React/src/pages/Home/components/HeroSection.tsx
@@ -4,7 +4,23 @@ import React from 'react'
 import './style/herosection.scss'
 
 const HeroSection = () => {
-    return <div>HeroSection</div>
+    const divArray = [...Array(400).keys()]
+    return (
+        <section className='hero-section'>
+            <div className='background'>
+                {divArray.map((_, key) => {
+                    return (
+                        <div
+                            style={{ animationDelay: `${key * 3}ms` }}
+                            key={key}
+                            className='block'
+                        ></div>
+                    )
+                })}
+            </div>
+            <div className='title'></div>
+        </section>
+    )
 }
 
 export default HeroSection
