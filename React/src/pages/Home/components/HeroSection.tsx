6623c80247ad47cb263763731119bf1c5a786a7c
sadra <sadrataghavi1383@gmail.com>
2022-03-09 11:59:37 +0330

    new hero section elements

--- React/src/pages/Home/components/HeroSection.tsx
+++ React/src/pages/Home/components/HeroSection.tsx
@@ -11,6 +11,7 @@ const HeroSection = () => {
     return (
         <section className='hero-section'>
             <div className='background'>
+                <div className='background-color'></div>
                 {divArray.map((_, key) => {
                     return (
                         <div
@@ -24,7 +25,38 @@ const HeroSection = () => {
                     )
                 })}
             </div>
-            <div className='title'></div>
+            <div className='titles-container'>
+                <span className='animation-fade'>
+                    <div
+                        className='title_hero'
+                        style={{ animationDelay: '2.5s' }}
+                    >
+                        بی نهایت فیلم و سریال در گوشی، لب تاب و کامپیتور شما
+                    </div>
+                </span>
+                <div className='paragraph'>
+                    <span className='animation-fade'>
+                        <div
+                            className='title_small'
+                            style={{ animationDelay: '2.75s' }}
+                        >
+                            هرجا که هستید با هر دستگاهی که هستید
+                        </div>
+                    </span>
+                    <span className='animation-fade'>
+                        <div
+                            className='title_smaller'
+                            style={{ animationDelay: '3s' }}
+                        >
+                            آماده اید؟ ایمیل خود را وارید کنید تا عضویت خود را
+                            شروع کنید
+                        </div>
+                    </span>
+                </div>
+                <div className='register-bar'>
+                    <input type='text' name='examle@gmail.com' id='' />
+                </div>
+            </div>
             <div className='scroll-down'>
                 <div className='wrapper' style={{ animationDelay: '0s' }}>
                     <span></span>
