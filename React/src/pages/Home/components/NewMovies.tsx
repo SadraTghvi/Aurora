261b9558bdf81915b3d10e54ef8dd6159392996c
sadra <sadrataghavi1383@gmail.com>
2022-03-10 23:01:37 +0330

    updated new movies sectionn

--- React/src/pages/Home/components/NewMovies.tsx
+++ React/src/pages/Home/components/NewMovies.tsx
@@ -6,10 +6,10 @@ import './style/newmovies.scss'
 const NewMovies = () => {
     return (
         <div className='new-movies'>
-            <div className='new-movies-header'></div>
+            <div className='new-movies-header glow-animation'></div>
             <div className='new-movies-column-container'>
-                <div className='new-movies-column'></div>
-                <div className='new-movies-column'></div>
+                <div className='new-movies-column glow-animation'></div>
+                <div className='new-movies-column glow-animation'></div>
             </div>
             <div className='new-movies-slider'></div>
         </div>
