c4ad63e056cbfc87aa2399557be063309b4e4123
sadra <sadrataghavi1383@gmail.com>
2022-03-10 16:41:29 +0330

    added new section to home page

--- React/src/pages/Home/index.tsx
+++ React/src/pages/Home/index.tsx
@@ -5,11 +5,13 @@ import './style/home.scss'
 
 // sections
 import HeroSection from './components/HeroSection'
+import NewMovies from './components/NewMovies'
 
 const Home = () => {
     return (
         <main className='home'>
             <HeroSection />
+            <NewMovies />
         </main>
     )
 }
