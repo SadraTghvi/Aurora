31cf417520cf7e6e4540880afbd4852649801c92
sadra <sadrataghavi1383@gmail.com>
2022-06-10 00:16:56 +0430

    linked products page to home page

--- React/src/pages/Home/index.tsx
+++ React/src/pages/Home/index.tsx
@@ -6,12 +6,14 @@ import './style/home.scss'
 // sections
 import HeroSection from './components/HeroSection'
 import LastOffers from './components/LastOffers'
+import Products from './components/Products'
 
 const Home = () => {
     return (
         <main className='home'>
             <HeroSection />
             <LastOffers />
+            <Products />
         </main>
     )
 }
