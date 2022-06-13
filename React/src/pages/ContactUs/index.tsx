b29531d50ed4ee4e8c37be3983ee084ce8fc4d2e
sadra <sadrataghavi1383@gmail.com>
2022-06-13 18:04:17 +0430

    improved contact us jsx

--- React/src/pages/ContactUs/index.tsx
+++ React/src/pages/ContactUs/index.tsx
@@ -5,12 +5,31 @@ import './style/contactus.scss'
 
 // sections
 import ContactHero from './ContactHero'
+import ContactForm from './ContactForm'
+
+// utils
+import UnderlineText from '../../components/UnderlineText'
+
+// icons
+import { MdEmail } from '@react-icons/all-files/md/MdEmail'
 
 const ContactUs = () => {
     return (
         <div className='contact-container'>
             <ContactHero />
-            <div className='contact-wrapper'></div>
+            <div className='contact-wrapper'>
+                <UnderlineText threshold={1}>
+                    <div className='icon'>
+                        <MdEmail />
+                    </div>
+                    <div className='holder title'>ارتباط الکترونیکی</div>
+                    <div className='icon'>
+                        <MdEmail />
+                    </div>
+                </UnderlineText>
+
+                <ContactForm />
+            </div>
         </div>
     )
 }
