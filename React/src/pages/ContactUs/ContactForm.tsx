01c96d33888fa4620a73210662f71b743f402afd
sadra <sadrataghavi1383@gmail.com>
2022-06-13 18:02:40 +0430

    created contact form and started contact us form container

---
+++ React/src/pages/ContactUs/ContactForm.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+// style
+import './style/contactform.scss'
+
+const ContactForm = () => {
+    return <div className='contactform-container'></div>
+}
+
+export default ContactForm
