b3bdb2b8751647e492c05c71e63540ae347f8f94
sadra <sadrataghavi1383@gmail.com>
2022-06-17 17:54:27 +0430

    index file for product page created and worked on

---
+++ React/src/pages/Product/index.tsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react'
+
+// style
+import './style/product.scss'
+
+// sections
+import ProductShow from './ProductShow'
+
+const mainProduct = require('../../../../static/imgs/main-product.jpg')
+
+var imgs: string[] = []
+
+Array.from(Array(4).keys()).map((_, index) => {
+    let img = require(`../../../../static/imgs/product-show${index}.jpg`)
+    console.log(img)
+
+    imgs.push(img)
+})
+const Product = () => {
+    const [MainImg, setMainImg] = useState(mainProduct)
+    return (
+        <div className='product-container'>
+            <div className='product-wrapper'>
+                <div className='product-info'></div>
+            </div>
+            <ProductShow
+                MainImg={MainImg}
+                setMainImg={setMainImg}
+                imgs={imgs}
+            />
+        </div>
+    )
+}
+
+export default Product
